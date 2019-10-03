72e3420d2197812adf550eceed4ccd9eb31e0439
Mauro <maurogarcia0209@gmail.com>
2019-10-02 23:56:39 -0300

    ng-tailwind configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ThreeColumnsGridComponent } from './three-columns-grid/three-columns-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThreeColumnsGridComponent } from './three-columns-grid/three-columns-grid.component';
+import { GridItemComponent } from './grid-item/grid-item.component';
+import { ColorfulNoteComponent } from './colorful-note/colorful-note.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreeColumnsGridComponent,
+    GridItemComponent,
+    ColorfulNoteComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
